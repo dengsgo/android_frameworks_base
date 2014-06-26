79acfdd2bb82afce7cc213f7404affe18b9262bc
zerojim kylin <kylinmod@gmail.com>
2014-06-26 19:30:58 +0800

    [1/2] Base: Lockscreen Notifications (1/2)
    Following commits inside
    AOSPAL/android_frameworks_base@3487cda
    AOSPAL/android_frameworks_base@5873e99
    AOSPAL/android_frameworks_base@7350e91
    AOSPAL/android_frameworks_base@8d99eed
    AOSPAL/android_frameworks_base@181110c
    AOSPAL/android_frameworks_base@ce14cdd
    AOSPAL/android_frameworks_base@3914e8d
    AOSPAL/android_frameworks_base@32ec14f
    AOSPAL/android_frameworks_base@6d2f41d
    AOSPAL/android_frameworks_base@b758e16
    AOSPAL/android_frameworks_base@6ba6fdb
    AOSPAL/android_frameworks_base@b20bdc9
    AOSPAL/android_frameworks_base@25f75c6
    AOSPAL/android_frameworks_base@a74309e
    AOSPAL/android_frameworks_base@5b6e143
    AOSPAL/android_frameworks_base@e9dcb99
    AOSPAL/android_frameworks_base@4caa49c
    AOSPAL/android_frameworks_base@0f2377c
    AOSPAL/android_frameworks_base@7e75b49
    AOSPAL/android_frameworks_base@bc6c83f Thanks @Der-Schubi
    AOSPAL/android_frameworks_base@8fd230d
    AOSPAL/android_frameworks_base@e05eba6
    AOSPAL/android_frameworks_base@082774a (Partialy ported - Removed codes
    of clear notifications navbar button)
    AOSPAL/android_frameworks_base@a6f92e5
    AOSPAL/android_frameworks_base@c3d76f5
    AOSPAL/android_frameworks_base@5b1d884
    AOSPAL/android_frameworks_base@0c0b133
    AOSPAL/android_frameworks_base@73a6b7d
    Lockscreen notifications: Add "show all notifications" navbar button
    Lockscreen Notifications: dismiss all button, improve animations,
    cleanup
    Lockscreen Notifications: some fixes
    Lockscreen Notifications: fix show/hide of show all button
    Keyguard: LN: fix and harden against null pointer exceptions
    [Base] Disbale LN by default
    Lockscreen Notifications: fix permission error
    Lockscreen Notifications: add hdpi, mdpi nav resources

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3449,6 +3449,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         if (false) Log.v(TAG, "updateResources");
     }
 
+    @Override
+    public void setButtonDrawable(int buttonId, int iconId) {
+        mNavigationBarView.setButtonDrawable(buttonId, iconId);
+    }
+
     //
     // tracing
     //
